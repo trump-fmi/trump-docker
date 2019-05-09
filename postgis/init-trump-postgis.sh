02e8081014ba709471c29b20d5d111a2ef167794
Michel Weitbrecht <>
2019-05-10 01:03:06 +0200

    postgis: Clean up and add metadata

--- postgis/init-trump-postgis.sh
+++ postgis/init-trump-postgis.sh
@@ -2,3 +2,4 @@
 set -e
 psql -U osm -c "CREATE EXTENSION hstore; CREATE EXTENSION postgis;" -d gis
 osm2pgsql --slim -U osm -d gis --hstore -S openstreetmap-carto-$CARTO_VERSION/openstreetmap-carto.style baden-wuerttemberg-latest.osm.pbf
+rm baden-wuerttemberg-latest.osm.pbf
