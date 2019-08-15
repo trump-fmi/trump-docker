999ae907b8d1e1dec792db8f74426dbfe4ebaec8
Michel Weitbrecht <>
2019-08-16 00:43:45 +0200

    label: Fix two labelserver project paths

--- label/entrypoint.sh
+++ label/entrypoint.sh
@@ -3,6 +3,6 @@
 # turn on bash's job control
 set -m
 export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib:/home/osm/modular_osm_label_server/lib
-cd /home/osm/osm_label_server
+cd /home/osm/modular_osm_label_server
 sudo -u osm /home/osm/modular_osm_label_server/start.sh &
 exec bash
