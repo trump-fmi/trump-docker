a7c1ff64c4052c0f590b42f06ee68cbd90695e5f
Michel Weitbrecht <>
2019-08-22 18:58:52 +0200

    label: Use script to run pipeline

--- label/entrypoint.sh
+++ label/entrypoint.sh
@@ -2,7 +2,16 @@
 
 # turn on bash's job control
 set -m
+# Exit on errors
+set -e
+
+if [ ! -f /home/osm/labels/labels.osm.pbf.ce ]; then
+    echo "/home/osm/labels/labels.osm.pbf.ce does not exist. Running label pipeline."
+    /usr/local/bin/update-labels
+fi
+
 export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib:/home/osm/modular_osm_label_server/lib
 cd /home/osm/modular_osm_label_server
 sudo -u osm /home/osm/modular_osm_label_server/start.sh &
+
 exec bash
---
+++ label/update-labels.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+wget -O /home/osm/input.osm.pbf "$OSM_INPUT_URL"
+cd /home/osm/label_pipeline
+./bin/pipeline.sh ../config/example.conf /home/osm/input.osm.pbf
+mv example_labeling.ce /home/osm/labels/labels.osm.pbf.ce
+rm /home/osm/input.osm.pbf
