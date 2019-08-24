6ec0b2f1b19fa78cf298cd1c7011e898fac44f3a
Michel Weitbrecht <>
2019-08-24 13:51:08 +0200

    pull.sh: Also fetch git and restart after changes

--- pull.sh
+++ pull.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
+git fetch;
+LOCAL=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse @{u})
 
 old_sum=$(docker images | sha256sum)
 echo "Pulling docker-compose images..."
 docker-compose pull -q
 new_sum=$(docker images | sha256sum)
 
-if [ "$old_sum" != "$new_sum" ]; then
-	echo "Images have been changed ($old_sum vs $new_sum). Restarting trump service."
-	systemctl restart trump
-	docker images prune
+if [[ ("$old_sum" != "$new_sum") || ("$LOCAL" != "$REMOTE") ]]; then
+    echo "Images or git changed ($old_sum vs $new_sum). Restarting trump service."
+    systemctl restart trump
+    docker images prune
 else
-	echo "Images have not been changed. Doing nothing".
+    echo "Images have not been changed. Doing nothing".
 fi
