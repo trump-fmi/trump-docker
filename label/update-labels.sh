cce89799b5d158b8c5add94416753951bcd07dd2
Michel Weitbrecht <>
2019-09-02 16:23:27 +0200

    label: Try buster, don't compile cmake, run pipeline in image

--- label/update-labels.sh
+++ label/update-labels.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-wget -O /home/osm/input.osm.pbf "$OSM_INPUT_URL"
+wget -O /home/osm/input.osm.pbf "${OSM_INPUT_URL-https://download.geofabrik.de/europe/germany-latest.osm.pbf}"
 cd /home/osm/label_pipeline
 ./bin/pipeline.sh ../config/example.conf /home/osm/input.osm.pbf
 mv example_labeling.ce /home/osm/labels/labels.osm.pbf.ce
