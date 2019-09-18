ba0de16781ea842d423dbc4d3985499ae8f93c32
Michel Weitbrecht <>
2019-09-18 17:39:07 +0200

    label: Use forked label_pipeline; clean up

--- label/update-labels.sh
+++ label/update-labels.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-wget -O /home/osm/input.osm.pbf "${OSM_INPUT_URL-https://download.geofabrik.de/europe/germany-latest.osm.pbf}"
+wget -O /home/osm/labels/input.osm.pbf "${OSM_INPUT_URL-https://download.geofabrik.de/europe/germany-latest.osm.pbf}"
+wget -O /home/osm/labels/pipeline.conf "${LABEL_CONFIG_URL-https://raw.githubusercontent.com/SlothOfAnarchy/label_pipeline/master/config/example.conf}"
 cd /home/osm/label_pipeline
-./bin/pipeline.sh ../config/example.conf /home/osm/input.osm.pbf
-mv example_labeling.ce /home/osm/labels/labels.osm.pbf.ce
-rm /home/osm/input.osm.pbf
+./bin/pipeline.sh /home/osm/labels/pipeline.conf /home/osm/labels/input.osm.pbf
+mv *.ce /home/osm/labels/labels.osm.pbf.ce
+rm /home/osm/labels/input.osm.pbf
