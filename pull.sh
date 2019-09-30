fd3ee2b8194f1a9bd8d98e37c317d86899236a2e
Michel Weitbrecht <>
2019-09-30 16:43:36 +0200

    systemd: Add reload method for trump; use it in trump-pull

--- pull.sh
+++ pull.sh
@@ -9,8 +9,8 @@ docker-compose pull -q
 new_sum=$(docker images | grep trump | sha256sum)
 
 if [[ ("$old_sum" != "$new_sum") || ("$LOCAL" != "$REMOTE") ]]; then
-    echo "Images or git changed ($old_sum vs $new_sum). Restarting trump service."
-    systemctl restart trump
+    echo "Images or git changed ($old_sum vs $new_sum). Reloading trump service."
+    systemctl reload trump
     docker system prune -f
 else
     echo "Images have not been changed. Doing nothing".
